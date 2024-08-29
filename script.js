fc0f90d9f3dddce727268a3910adb97085e7616b
anjali bansal <anjalibansal@anjalis-MBP.bbrouter>
2024-08-29 15:34:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// var dulha = "Lab";
+// var dulhan = "Laby";
+
+// function abcd(){
+//     for(let i = 1 ; i<12 ; i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+
+// abcd();
+
+
+var a =12;
+
+let b = 12;
